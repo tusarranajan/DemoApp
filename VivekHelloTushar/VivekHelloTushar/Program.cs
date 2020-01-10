fcab78775ea5f5d616d49afe6b7c729e23167323
tusarranajan <tbtusar7@gmail.com>
2020-01-10 17:33:47 +0530

    Update Program.cs

--- VivekHelloTushar/VivekHelloTushar/Program.cs
+++ VivekHelloTushar/VivekHelloTushar/Program.cs
@@ -11,7 +11,7 @@ namespace VivekHelloTushar
         static void Main(string[] args)
         {
             Console.Write("today 2019- dd-mm--hh--yyyy");
-            Console.Write("Hello");
+            Console.Write("Hello world");
             
         }
     }
